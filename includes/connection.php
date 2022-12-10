6a958250abad860c22af01ba53597849c5b6ee4b
Gabby <gabrielalvaro693@gmail.com>
2022-12-10 19:16:35 +0800

    updated db connection

--- includes/connection.php
+++ includes/connection.php
@@ -1,5 +1,5 @@
 <?php
- $db = mysqli_connect('localhost', 'root', '') or
+ $db = mysqli_connect('ftp.geaa.tech', 'u180008257_ims', 'iMSPASSWORD1') or
         die ('Unable to connect. Check your connection parameters.');
-        mysqli_select_db($db, 'Final_InventorySystem' ) or die(mysqli_error($db));
+        mysqli_select_db($db, 'u180008257_ims' ) or die(mysqli_error($db));
 ?>
